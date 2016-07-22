31e252d10820cb424529adbc26c8281ee02ee62c
Linda Speight <lspeight@ellsworthAmerican.com>
2016-07-22 13:12:20 -0400

    change slideshow images
    activities, calendar, contact & map pages

--- site/_vti_cnf/banner.js
+++
@@ -1,7 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|21 Apr 2015 13:18:46 -0000
-vti_extenderversion:SR|6.0.2.8161
-vti_backlinkinfo:VX|contactus.html rates.html calendar.html contact.html gallery.html thanks.html activities.html about.html information.html rates-2015.html map-13Aug15.html rates\\ -\\ Copy.html cabins.html index.html map.html trailer.html
-vti_author:SR|BAKERSVILLE\\JaneB
-vti_modifiedby:SR|BAKERSVILLE\\JaneB
-vti_timecreated:TR|21 Apr 2015 13:24:56 -0000
